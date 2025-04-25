c09007d7c5d5a8f8d629cb5c264044e2810b1ab5
iRatzeyMezeri <125797032+iRatzeyMezeri@users.noreply.github.com>
2025-04-25 19:52:45 +0200

    Create sqserpuloPlanets.java

---
+++ src/SettingQSerpulo/sqserpuloPlanets.java
@@ -0,0 +1,79 @@
+package mindustry.content;
+
+import arc.func.*;
+import arc.graphics.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.game.*;
+import mindustry.graphics.*;
+import mindustry.graphics.g3d.*;
+import mindustry.graphics.g3d.PlanetGrid.*;
+import mindustry.maps.planet.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.meta.*;
+
+public class Planets{
+    public static Planet
+    sunq,
+    serpuloq,
+
+    public static void load(){
+        sunq = new Planet("sun (Q)", null, 4f){{
+            bloom = true;
+            accessible = false;
+
+            meshLoader = () -> new SunMesh(
+                this, 4,
+                5, 0.3, 1.7, 1.2, 1,
+                1.1f,
+                Color.valueOf("ff7a38"),
+                Color.valueOf("ff9638"),
+                Color.valueOf("ffc64c"),
+                Color.valueOf("ffc64c"),
+                Color.valueOf("ffe371"),
+                Color.valueOf("f4ee8e")
+            );
+        }};
+
+       
+        serpuloq = new Planet("Serpulo (Q)", sunq, 1f, 3){{
+            generator = new SerpuloPlanetGenerator();
+            meshLoader = () -> new HexMesh(this, 6);
+            cloudMeshLoader = () -> new MultiMesh(
+                new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Pal.spore).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
+                new HexSkyMesh(this, 1, 0.6f, 0.16f, 5, Color.white.cpy().lerp(Pal.spore, 0.55f).a(0.75f), 2, 0.45f, 1f, 0.41f)
+            );
+
+            launchCapacityMultiplier = 0.5f;
+            sectorSeed = 2;
+            allowWaves = true;
+            allowLegacyLaunchPads = false;
+            allowWaveSimulation = false;
+            allowSectorInvasion = true;
+            allowLaunchSchematics = false;
+            enemyCoreSpawnReplace = true;
+            allowLaunchLoadout = true;
+            //doesn't play well with configs
+            prebuildBase = false;
+            ruleSetter = r -> {
+                r.waveTeam = Team.crux;
+                r.placeRangeCheck = false;
+                r.showSpawns = true;
+                r.coreDestroyClear = true;
+            };
+            showRtsAIRule = true;
+            iconColor = Color.valueOf("7d4dff");
+            atmosphereColor = Color.valueOf("3c1b8f");
+            atmosphereRadIn = 0.02f;
+            atmosphereRadOut = 0.3f;
+            startSector = 15;
+            alwaysUnlocked = true;
+            allowSelfSectorLaunch = true;
+            landCloudColor = Pal.spore.cpy().a(0.5f);
+        }};
+    }
+
+}

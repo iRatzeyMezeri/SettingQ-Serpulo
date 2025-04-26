faee0793f1a1239de11d7baa309de3114eee0688
iRatzeyMezeri <125797032+iRatzeyMezeri@users.noreply.github.com>
2025-04-26 12:13:18 +0200

    Update SettingQSerpulo.java

--- src/sqserpulo/SettingQSerpulo.java
+++ src/sqserpulo/SettingQSerpulo.java
@@ -29,6 +29,7 @@ public class SettingQSerpulo extends Mod{
     public void loadContent(){
         // sqserpuloPlanets.load();
         sqserpuloItems.load();
+        sqserpuloUnits.load();
     }
 
 }

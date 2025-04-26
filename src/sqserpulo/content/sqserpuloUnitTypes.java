14de631d0ef522dd9217b2af30997b92bd289127
iRatzeyMezeri <125797032+iRatzeyMezeri@users.noreply.github.com>
2025-04-26 12:01:32 +0200

    Create sqserpuloUnitTypes.java

---
+++ src/sqserpulo/content/sqserpuloUnitTypes.java
@@ -0,0 +1,78 @@
+package sqserpulo.content;
+
+import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.Seq;
+import mindustry.*;
+import mindustry.ai.types.*;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.entities.abilities.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.effect.*;
+import mindustry.entities.part.*;
+import mindustry.entities.pattern.*;
+import mindustry.gen.*;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Layer;
+import mindustry.graphics.Pal;
+import mindustry.type.*;
+import mindustry.type.weapons.*;
+
+public class sqserpuloUnitTypes {
+
+    public static UnitType
+
+    //Core
+    // alphaq, betaq, gammaq,
+
+    // Ground Specialist
+    // demadian, atraxq, spiroctq, arkyidq, toxopidq
+
+    // Ground Assault
+    daggerq, maceq, // staff, scepterq, stormbow,
+
+    // Ground Support
+    // novaq, pulsarq, quasarq, velaq, ursa,
+
+    // Air Assault
+    flareq, // horizonq, zenithq, antumbraq, eclipseq,
+
+    // Air Support
+    // mono, poly, mega, tera, exa,
+
+    // Naval Assault
+    // rissoq, minkeq, brydeq, seiq, omuraq,
+
+    // Naval Support
+    // retusaq, oxynoeq, cyerceq, aegiresq, navanaxq;
+  
+   public static void load() {
+      daggerq = new UnitType("daggerq"){{
+            researchCostMultiplier = 0.5f;
+            speed = 0.5f;
+            hitSize = 8f;
+            health = 150;
+            weapons.add(new Weapon("daggerq-weapon"){{
+                reload = 13f;
+                x = 4f;
+                y = 2f;
+                top = false;
+                ejectEffect = Fx.casing1;
+                bullet = new BasicBulletType(2.5f, 9){{
+                    backColor = Color.valueOf("#ab98d2");
+                    frontColor = Color.valueOf("#ffffff");
+                    trailColor = Color.valueOf("#8c7ea8");
+                    sprite = "sqserpulo-leadbullet-front"
+                    backSprite = "sqserpulo-leadbullet-back"
+                    width = 7f;
+                    height = 9f;
+                    lifetime = 60f;
+                }};
+            }});
+        }};
+  }
+}
